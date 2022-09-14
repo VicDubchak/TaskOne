51d5c4793975edeaf5c475e2760356388ee176b7
Vic Dubchak <vdubchak@mac.com>
2022-09-14 18:40:34 +0300

    Create New Project M16_1 for <Task One>

---
+++ M16_1/M16_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16_1
+//
+//  Created by Виктор Дубчак on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
